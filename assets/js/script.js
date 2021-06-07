4adba834cb08def886a02474b48524e524aea15f
Asa Grace <asag182@gmail.com>
2021-06-06 19:09:22 -0600

    current weather of selected city displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var button = document.querySelector('.btn')
 var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
+var cityName = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
@@ -8,9 +8,15 @@ button.addEventListener('click' , function() {
     fetch(
         'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=e12acf058398663e5a5c79e83134f180'
     )
-        .then(response => response.json()
-        )
-        .then(data => console.log(data)
-        )
+        .then(response => response.json())
         
+        .then(data => {   
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+        
+        cityName.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+        })
 })
