cf2e054a921b0d261cff98d9b2988629a917766f
Asa Grace <asag182@gmail.com>
2021-06-06 20:09:02 -0600

    5-day forecast in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ var inputValue = document.querySelector('.inputValue')
 var cityName = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
+var forecastSymbol = document.querySelector('forecast')
 
-button.addEventListener('click' , function() {
+button.addEventListener('click', function() {
     fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=e12acf058398663e5a5c79e83134f180'
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        inputValue.value +
+        '&appid=e12acf058398663e5a5c79e83134f180&units=imperial' +
+        
+        'https://api.openweathermap.org/data/2.5/forecast?id='+ 
+        inputValue + 
+        '&appid=e12acf058398663e5a5c79e83134f180&units=imperial&forcast.symbol=forecast.symbol.var'
     )
         .then(response => response.json())
         
@@ -14,9 +21,11 @@ button.addEventListener('click' , function() {
         var nameValue = data['name'];
         var tempValue = data['main']['temp'];
         var descValue = data['weather'][0]['description'];
+        var symbolValue = data['forecast.symbol'];
         
         cityName.innerHTML = nameValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
+        temp.innerHTML = tempValue + ' °F ';
+        desc.innerHTML = descValue + '';
+        forecastSymbol.innerHTML = symbolValue;
         })
 })
